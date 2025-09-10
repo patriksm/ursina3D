4df9151df760a229f5aa999b9cef65aa17ee5503
Vito-Soul <solovjovs.vitalijs@gmail.com>
2025-09-10 18:49:55 +0300

    zimes

--- VS/3dmyversion.py
+++ VS/3dmyversion.py
@@ -4,6 +4,8 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+window.fullscreen = True # 05 pilnā ekrāna režīms
+
 player = FirstPersonController(
     height=3,
     speed=15,
@@ -202,7 +204,7 @@ score_text = Text(
     scale = 2, #zimes izmers
     position = (-0.75, 0.45), # zimes pozicija
     origin=(0, 0),
-    color=color.green
+    color=color.red
 )
 
 for block in block_list:
@@ -255,14 +257,14 @@ goal = Entity(#finish
 pillar = Entity( # stabs
     model='cube',
     texture='assets/metal_plate_diff_1k.jpg',
-    position=(0, 36, 58),
-    scale=(1, 50, 1)
+    position=(0, 24, 58),
+    scale=(1, 25, 1)
 )
 
 mySphere = Entity(  #sfera 
     texture='assets/box_profile_metal_sheet_diff_1k.jpg',
     model='sphere',
-    position=(0, 60, 58),
+    position=(0, 35, 58),
     scale=(20, 20, 20)
 )
 
@@ -320,11 +322,12 @@ def update():
         if block.intersects().hit:
             player.x -= directions[i] * time.dt * speed_multiplier
         i += 1
+
         #if block.enabled: #bloka griesana
             #block.rotation_y += 100 * time.dt
 
         if mySphere.enabled: #sferas griesana
-            mySphere.rotation_y += 100 * time.dt
+            mySphere.rotation_y += 10 * time.dt
 
         #if game_over_text.enabled:
             #game_over_text.rotation_y +=20 * time.dt
@@ -361,4 +364,29 @@ def input(key):  # 02 ja nospiests kāds taustiņš, šeit var programmēt darb�
         player.speed = 40   # speletaja paatrinasana
     else:
         player.speed = 15
+
+controls_text = Text(
+    text=(
+        "Esc : Quit\n"
+        "Q : Restart\n"
+        "Shift : Run\n"
+        "W : Front\n"
+        "S : Back\n"
+        "A : Left\n"
+        "D : Right\n"
+        "Space : Jump"
+    ),
+    position = (-0.80, -0.35),  # koordinates
+    scale=1.2,
+    origin=(0, 0),
+    color=color.red
+)
+
+secret_level_text = Text(
+    text="Find Secret Level",
+    position = (0, 0.45),
+    scale=2,       
+    color=color.red,
+    origin=(0, 0)  
+)
 app.run() # 00 palaižām spēles logu
